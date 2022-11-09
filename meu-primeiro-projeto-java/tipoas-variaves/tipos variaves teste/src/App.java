d09e7fa6c145f091e7328fcfa8db2f5b93dcc744
marialeticias <mdpayne33@gmail.com>
2022-11-09 17:13:39 -0300

    atualização 2 do teste de veriavel

---
+++ meu-primeiro-projeto-java/tipoas-variaves/tipos variaves teste/src/App.java
@@ -0,0 +1,14 @@
+public class App {
+    /**
+     * @param args
+     * @throws Exception
+     */
+    public static void main(String[] args) throws Exception {
+        final short numeroCurto = 1;
+        int numero = 5;
+
+        numero = 10;
+
+        System.out.print(numero);
+    }
+}
